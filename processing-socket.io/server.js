fb4191cd0660d3ab80940aac09e7781c4456e40a
Michael Vogt <michu@neophob.com>
2012-07-22 13:48:24 +0200

    add new processing.js example

---
+++ processing-socket.io/server.js
@@ -0,0 +1,39 @@
+var GREPFILE = '/var/log/system.log';
+
+var http = require('http');
+var util = require('util');
+var spawn = require("child_process").spawn;
+var static = require('node-static');
+
+var file = new(static.Server)('./clientfiles/');
+var port = process.env.PORT || 8001;
+var server = http.createServer(function(request, response){
+	request.addListener('end', function () {
+        //
+        // Serve files!
+        //
+        file.serve(request, response);
+    });
+}).listen(port);
+
+// use socket.io
+var io = require('socket.io').listen(server);
+
+//turn off debug
+io.set('log level', 2);
+
+// define interactions with client
+io.sockets.on('connection', function(socket){   	   
+    console.log('connection!');
+
+	setInterval(function() {
+	    console.log('send new trigger');
+	    socket.emit('bang', {'x': Math.random(), 'y': Math.random() });	
+	}, 1000 );
+
+	socket.on('disconnect', function () {
+		console.log('disconnected');
+	});
+}); 
+  
+console.log('Server started on port '+port);
