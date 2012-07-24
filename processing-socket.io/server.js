5221996dc424fa702f92061d60a69577dd19ed4c
Michael Vogt <michu@neophob.com>
2012-07-24 09:07:54 +0200

    implement my first callback method

--- processing-socket.io/server.js
+++ processing-socket.io/server.js
@@ -18,58 +18,51 @@ var server = http.createServer(function(request, response){
 
 var newDataEmitter = new events.EventEmitter();
 
+//grab some raw data
+function getJsonData(option, callback) {
+  var req = http.request(option, function(res) {
+    var body = '';
+    res.setEncoding('utf8');
+    res.on('data', function (chunk) {
+        body += chunk;
+    });
+    res.on('end', function() {
+        callback(body);
+    });
+  });
+  req.on('error', function(e) {
+      console.log('problem with request: ' + e.message);
+  });
+  req.end();  
+}
+
 //TODO make async function to get json data, handle response in a callback
 function getHistoryAareData() { 	
-	var req = http.request({ host: 'aare.schwumm.ch', path: '/api/archive' }, function(res) {
-		var body = '';
-  		res.setEncoding('utf8');
-  		res.on('data', function (chunk) {
-    		body += chunk;
-    		//console.log(body);
-  		});
-	    res.on('end', function() {
-      		var aareData = JSON.parse(body);
+  getJsonData({ host: 'aare.schwumm.ch', path: '/api/archive' }, function(body) {
+    var aareData = JSON.parse(body);
 
-          var date = aareData.data.datetime;
-          var values = aareData.data.temperature;
+    var date = aareData.data.datetime;
+    var values = aareData.data.temperature;
 
-          for(var i = 0; i < date.length; i++) {
-            if (date[i] !== null && values[i] !== null) {
-              var sendData = {'temperature': values[i], 'date': date[i]};
-              console.log(sendData);
-              newDataEmitter.emit('bang', sendData);
-            }            
-          }
-      		//TODO
-    	});
-	});
-	req.on('error', function(e) {
-  		console.log('problem with request: ' + e.message);
-	});
-	req.end();	
+    for(var i = 0; i < date.length; i++) {
+       if (date[i] !== null && values[i] !== null) {
+         var sendData = {'temperature': values[i], 'date': date[i]};
+         console.log(sendData);
+         newDataEmitter.emit('bang', sendData);
+       }            
+    }
+  });
 }
 
 
 function getCurrentAareData() { 
-	var req = http.request({ host: 'aare.schwumm.ch', path: '/aare.json' }, function(res) {
-		var body = '';
-  		res.setEncoding('utf8');
-  		res.on('data', function (chunk) {
-    		body += chunk;
-  		});
-	    res.on('end', function() {
-          console.log(body);
-      		var aareData = JSON.parse(body);
-            if (aareData.temperature > 0) {  
-                console.log(util.inspect(aareData));
-                newDataEmitter.emit('bang', aareData);
-            }  
-    	});
-	});
-	req.on('error', function(e) {
-  		console.log('problem with request: ' + e.message);
-	});
-	req.end();	
+  getJsonData({ host: 'aare.schwumm.ch', path: '/aare.json' }, function(body) {  
+    var aareData = JSON.parse(body);
+    if (aareData.temperature > 0) {  
+        console.log(util.inspect(aareData));
+        newDataEmitter.emit('bang', aareData);
+    }  
+  });
 }
 //setInterval(getCurrentAareData, 1000);
 setInterval(getHistoryAareData, 3000);
