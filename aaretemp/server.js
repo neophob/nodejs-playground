cbd8c1a3d43b37b011dd3ee9f1e73e2595ea3993
Michael Vogt <michu@neophob.com>
2012-07-26 14:21:21 +0200

    start implementing cache

--- aaretemp/server.js
+++ aaretemp/server.js
@@ -8,6 +8,8 @@ var events = require('events');
 var file = new(staticServ.Server)('./aaretemp//clientfiles/');
 var port = process.env.PORT || 8001;
 
+var MAX_CACHE_ELEMENTS = 128;
+
 var server = http.createServer(function(request, response){
 	request.addListener('end', function () {
         //
@@ -16,8 +18,6 @@ var server = http.createServer(function(request, response){
         file.serve(request, response);
     });
 }).listen(port);
-
-var newDataEmitter = new events.EventEmitter();
 
 //grab some raw data
 function getJsonData(option, callback) {
@@ -37,7 +37,11 @@ function getJsonData(option, callback) {
   req.end();  
 }
 
-//TODO make async function to get json data, handle response in a callback
+var cache = new Array();
+
+/*
+ * fill internal cache with data from the last day
+ */
 function getHistoryAareData() {
   console.log('fetch initial data');
   getJsonData({ host: 'aare.schwumm.ch', path: '/api/archive' }, function(body) {
@@ -46,18 +50,41 @@ function getHistoryAareData() {
     var date = aareData.data.datetime;
     var values = aareData.data.temperature;
 
+    var cnt=0;
     for(var i = 0; i < date.length; i++) {
        if (date[i] !== null && values[i] !== null) {
+         //parse data from string, its GMT+1
          var sendData = {'temperature': values[i], 'date': new Date(Date.parse(date[i], "yyyy-MM-dd HH:mm:ss"))};
          console.log(sendData);
-         newDataEmitter.emit('bang', sendData);
+         //newDataEmitter.emit('bang', sendData);
+         cache[cnt++] = sendData;
        }            
     }
-    console.log('initial data fetch complete...');
+    console.log('initial data fetch complete, found '+cache.length+' entries');
   });
 }
 
+
+/*
+ * send cached entries to the frontend
+ */
+function sendCacheToClient(socket) {
+  console.log('send cache data to client, entries: '+cache.length);
+  for(var i = 0; i < cache.length; i++) {
+    //var sendData = {'temperature': cache[i].temperature, 'date': cache[i].date}    
+    //newDataEmitter.emit('bang', sendData);
+    socket.emit('bang', {'temp': cache[i].temperature, 'date': cache[i].date });
+  }
+  console.log('cache sent');
+}
+
+
+
 var lastDate=0;
+var newDataEmitter = new events.EventEmitter();
+/*
+ * get current data and store it into the cache and send it to the client
+ */
 function getCurrentAareData() { 
   getJsonData({ host: 'aare.schwumm.ch', path: '/aare.json' }, function(body) {  
     var aareData = JSON.parse(body);
@@ -66,15 +93,19 @@ function getCurrentAareData() {
     var newTimestamp  = new Date(Date.parse(aareData.date,"yyyy-MM-dd HH:mm:ss"));
     if (aareData.temperature > 0 && newTimestamp>lastDate) {          
         var sendData = {'temperature': aareData.temperature, 'date': newTimestamp};
-        console.log(sendData);
-        newDataEmitter.emit('bang', sendData);        
-        lastDate = newTimestamp;        
+        newDataEmitter.emit('bang', sendData);
+        console.log('new data found: '+JSON.stringify(sendData.date));
+        lastDate = newTimestamp;
+
+        //TODO store into cache
     }  
   });
 }
 
-//get initial data
-//setTimeout(getHistoryAareData(), 1000);
+
+//cache data
+getHistoryAareData();
+
 //get up to date data
 setInterval(getCurrentAareData, 6000);
 
@@ -89,14 +120,16 @@ io.set('log level', 2);
 io.sockets.on('connection', function(socket){   	   
     console.log('socket connection!');
 
-    getHistoryAareData();
+    sendCacheToClient(socket);
 
     newDataEmitter.addListener('bang', function (aareData) {
+      //console.log('socketsend '+JSON.stringify(aareData));
     	socket.emit('bang', {'temp': aareData.temperature, 'date': aareData.date });	
 	});
 
 	socket.on('disconnect', function () {
 		console.log('socket disconnected');
+    socket
 	});
 }); 
   
